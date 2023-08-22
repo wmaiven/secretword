c4f613f3ae367449dfce294a1907f467558f2339
wmaiven <maivencunha@hotmail.com>
2023-08-22 20:23:33 -0300

    Adicionando o banco e dando uma limpada geral

---
+++ ReactSecretWord/src/data/words.js
@@ -0,0 +1,23 @@
+export const wordlist = { //"simulação de um banco de dados onde será pego as palavras"
+    carro: ["motor", "parabrisa", "freio", "volante", "acelerador", "retrovisor", "janela", "radio", "painel"],
+    frutas: ["maca", "banana", "laranja", "uva", "morango", "abacaxi", "manga", "kiwi"],
+    animais: ["cachorro", "gato", "leao", "elefante", "girafa", "tigre", "panda", "cavalo"],
+    cores: ["vermelho", "azul", "amarelo", "verde", "roxo", "laranja", "rosa", "preto", "branco"],
+    profissoes: ["medico", "professor", "engenheiro", "chef", "advogado", "artista", "bombeiro"],
+    alimentos: ["pizza", "hamburguer", "salada", "sorvete", "batatafrita", "sanduiche", "frango", "macarrao"],
+    esportes: ["futebol", "basquete", "natacao", "tenis", "corrida", "ciclismo", "ginastica"],
+    estacoes: ["primavera", "verao", "outono", "inverno"],
+    objetos: ["cadeira", "mesa", "computador", "telefone", "lampada", "livro", "oculos", "relogio"],
+    paises: ["Brasil", "EstadosUnidos", "Franca", "Japao", "China", "India", "Italia", "Alemanha"],
+    meiosdetransporte: ["aviao", "barco", "bicicleta", "metro", "onibus", "trem", "carro"],
+    planetas: ["Terra", "Marte", "Venus", "Jupiter", "Saturno", "Netuno", "Urano", "Plutao"],
+    instrumentosmusicais: ["violao", "piano", "flauta", "bateria", "violino", "saxofone", "trompete"],
+    roupas: ["camiseta", "calca", "vestido", "sapato", "jaqueta", "bone", "luvas", "meias"],
+    partesdocorpo: ["cabeca", "ombro", "joelho", "dedos", "olhos", "orelhas", "boca"],
+    numeros: ["um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez"],
+    moveis: ["sofa", "estante", "armario", "cama", "criadomudo", "poltrona"],
+    generosmusicais: ["rock", "pop", "jazz", "hiphop", "eletronica", "classica", "samba"],
+    utensiliosdecozinha: ["panela", "frigideira", "colher", "garfo", "faca", "prato", "copo"],
+    literatura: ["romance", "poesia", "contos", "fantasia", "ficcaocientifica", "drama"],
+    formasgeometricas: ["circulo", "triangulo", "quadrado", "retangulo", "hexagono", "octogono"]
+};
