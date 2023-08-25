8b430751c3cbb436f8f6c69cb0d1b9ac08489c79
wmaiven <maivencunha@hotmail.com>
2023-08-25 12:33:28 -0300

    estilo do start screen

--- ReactSecretWord/src/components/StartScreen.jsx
+++ ReactSecretWord/src/components/StartScreen.jsx
@@ -2,7 +2,7 @@ import './StartScreen.css';
 
 export const StartScreen = () => {
   return (
-    <div>
+    <div className='Start'>
         <h1>Jogo da Adivinhação</h1>
         <p>Clique no botão abaixo para começar a jogar</p>
         <button>Começar o jogo</button>
